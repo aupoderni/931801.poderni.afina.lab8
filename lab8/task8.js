0ffcb5c38783976cd92d395e4d60c77e69dd5795
aupoderni <72687246+aupoderni@users.noreply.github.com>
2020-12-11 14:15:47 +0700

    Add files via upload

---
+++ lab8/task8.js
@@ -0,0 +1,116 @@
+var n=0;
+var deliting = 0;
+var nRow = [];
+var empty;
+var l =0;
+var result ="";
+function addRow(m){
+	return`
+			<tr class="${m}">
+				<td><input type="text" class="field1"></td>
+				<td><input type="number" onKeyDown="if(this.value.length==3) return false;" class="field2"></td>
+				<td><button class="upButton">&#8593;</button></td>
+				<td><button class="downButton">&#8595;</button></td>
+				<td><button class="delButton">х</button></td>
+			</tr>	
+        `
+	
+}
+
+function addButton(){
+	n+=1;
+	deliting+=1;
+	var m = 'row' + n;
+	document.body.querySelector('table').insertAdjacentHTML("beforeend",  this.addRow(m));
+	nRow[n] = document.querySelector('.'+m)
+	nRow[Number(n)+1]=null;
+}
+
+function delButton(delObject){
+	
+	var i = delObject.parentElement.parentElement.className;
+	i = i.replace(/\D/g,'');
+	var j = nRow[n].className;
+	j = j.replace(/\D/g,'') - 1;
+	var m = n;
+	nRow.splice(i, 1);
+	document.body.querySelector('table').querySelector('.'+delObject.parentElement.parentElement.className).remove();
+	for (m;m>i;m--) {
+		nRow[m-1].className = 'row' + j;
+		j-=1;	
+	}
+	n-=1;
+}
+
+function downButton(down){
+	var i = down.parentElement.parentElement.className;
+	i = i.replace(/\D/g,'');
+	j = Number(i)+1;
+	if (nRow[j]!=null){
+		temp = document.body.querySelector('table').querySelector('.'+nRow[i].className).querySelector('.field1').value;
+		document.body.querySelector('table').querySelector('.'+nRow[i].className).querySelector('.field1').value = document.body.querySelector('table').querySelector('.'+nRow[j].className).querySelector('.field1').value;
+		document.body.querySelector('table').querySelector('.'+nRow[j].className).querySelector('.field1').value = temp;
+		temp = document.body.querySelector('table').querySelector('.'+nRow[i].className).querySelector('.field2').value;
+		document.body.querySelector('table').querySelector('.'+nRow[i].className).querySelector('.field2').value = document.body.querySelector('table').querySelector('.'+nRow[j].className).querySelector('.field2').value;
+		document.body.querySelector('table').querySelector('.'+nRow[j].className).querySelector('.field2').value = temp;
+	}
+}
+
+function upButton(up){
+	var i = up.parentElement.parentElement.className;
+	i = i.replace(/\D/g,'');
+	j = i - 1;
+	if (nRow[j]!=null){
+		temp = document.body.querySelector('table').querySelector('.'+nRow[i].className).querySelector('.field1').value;
+		document.body.querySelector('table').querySelector('.'+nRow[i].className).querySelector('.field1').value = document.body.querySelector('table').querySelector('.'+nRow[j].className).querySelector('.field1').value;
+		document.body.querySelector('table').querySelector('.'+nRow[j].className).querySelector('.field1').value = temp;
+		temp = document.body.querySelector('table').querySelector('.'+nRow[i].className).querySelector('.field2').value;
+		document.body.querySelector('table').querySelector('.'+nRow[i].className).querySelector('.field2').value = document.body.querySelector('table').querySelector('.'+nRow[j].className).querySelector('.field2').value;
+		document.body.querySelector('table').querySelector('.'+nRow[j].className).querySelector('.field2').value = temp;
+	}
+}
+function saveButton(){
+	var saveUpload = [];
+	if (l === 1){
+		d = document.querySelector('.saver');
+		document.body.removeChild(d);
+		result = "";
+	}
+	if (nRow[0]!=null){
+		l=1;
+
+	for (i=0;i<=n;i++){
+	saveUpload.field1 = document.body.querySelector('table').querySelector('.'+nRow[i].className).querySelector('.field1').value;
+		if (saveUpload.field1 == empty) { saveUpload.field1 = "вы не ввели слово!";}
+	saveUpload.field2  = document.body.querySelector('table').querySelector('.'+nRow[i].className).querySelector('.field2').value;
+		if (saveUpload.field2 == empty) { saveUpload.field2 = "вы не ввели число!";}
+	result = result + saveUpload.field1 + " : " + saveUpload.field2;
+	if (i!=n) {result = result + " , "};
+
+	
+	}
+		document.body.insertAdjacentHTML("beforeend",  `<div class="saver">{"${result}"}</div>`);
+	}
+}
+function listener(){
+	nRow[0]=document.querySelector('.row0');
+	empty = document.body.querySelector('table').querySelector('.row0').querySelector('.field1').value;
+	nRow[-1]=null;
+		document.body.addEventListener("click", e => {
+                if (e.target.classList.contains("delButton")) {
+					this.delButton(e.target);
+            }
+        });
+		document.body.addEventListener("click", e => {
+                if (e.target.classList.contains("downButton")) {
+					this.downButton(e.target);
+            }
+        });
+		document.body.addEventListener("click", e => {
+                if (e.target.classList.contains("upButton")) {
+					this.upButton(e.target);
+            }
+        });
+}
+
+document.addEventListener("DOMContentLoaded", () => listener());
